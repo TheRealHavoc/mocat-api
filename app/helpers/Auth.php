65b8b0d74de350285255246898ec8574b3f28bd6
Dakpaneel <bradleyoosterveen@gmail.com>
2020-04-27 19:17:50 +0200

    Update Auth.php

--- app/helpers/Auth.php
+++ app/helpers/Auth.php
@@ -8,16 +8,13 @@
 
             $request->headers = getallheaders();
 
-            $sql = $db->conn->prepare('SELECT `token` FROM `users` WHERE `username` = :username');
-            $sql->bindParam(':username', $request->headers['username']);
+            $sql = $db->conn->prepare('SELECT `token` FROM `users` WHERE `token` = :token');
+            $sql->bindParam(':token', $request->headers['token']);
 
             if(!$sql->execute())
                 Response::error("Something went wrong", 500);
 
             if(!$res = $sql->fetch())
-                Response::error("Something went wrong with the authentication", 400);
-
-            if($request->headers['token'] !== $res['token'])
                 Response::error("Something went wrong with the authentication", 400);
 
             return true;
